04100e7278e0af72df486f8e8c2693fc938548c2
Guido Klijn <guidoklijn42@gmail.com>
2025-07-26 23:20:53 +0200

    add unit tests

---
+++ csharp.xUnit/GildedRoseTests/GildedRose_PlusFiveDexterityVest.cs
@@ -0,0 +1,57 @@
+﻿using Xunit;
+using System.Collections.Generic;
+using GildedRoseKata;
+
+namespace GildedRoseTests;
+
+public class GildedRose_PlusFiveDexterityVest
+{
+    [Fact]
+    public void PlusFiveDexterityVest_Decreases_Quality_By_Day_By_One()
+    {
+        var plusFiveDexterityVest = new Item { Name = "+5 Dexterity Vest", SellIn = 2, Quality = 6 };
+        List<Item> items = [plusFiveDexterityVest];
+
+        var gildedRose = new GildedRose(items);
+        gildedRose.UpdateQuality();
+
+        Assert.Equal(5, plusFiveDexterityVest.Quality);
+    }
+
+    [Fact]
+    public void PlusFiveDexterityVest_Decreases_SellIn_By_Day_By_One()
+    {
+        var plusFiveDexterityVest = new Item { Name = "+5 Dexterity Vest", SellIn = 2, Quality = 6 };
+        List<Item> items = [plusFiveDexterityVest];
+
+        var gildedRose = new GildedRose(items);
+        gildedRose.UpdateQuality();
+
+        Assert.Equal(1,  plusFiveDexterityVest.SellIn);
+    }
+    
+    
+    [Fact]
+    public void PlusFiveDexterityVest_Decreases_Quality_After_SellIn_Gets_ZeroOrNegative_By_Two()
+    {
+        var plusFiveDexterityVest = new Item { Name = "+5 Dexterity Vest", SellIn = 0, Quality = 10 };
+        List<Item> items = [plusFiveDexterityVest];
+
+        var gildedRose = new GildedRose(items);
+        gildedRose.UpdateQuality();
+        
+        Assert.Equal(8, plusFiveDexterityVest.Quality);
+    }
+    
+    [Fact]
+    public void PlusFiveDexterityVest_Doesnt_Decrease_Quality_Beyond_Zero()
+    {
+        var plusFiveDexterityVest = new Item { Name = "+5 Dexterity Vest", SellIn = 0, Quality = 1 };
+        List<Item> items = [plusFiveDexterityVest];
+
+        var gildedRose = new GildedRose(items);
+        gildedRose.UpdateQuality();
+        
+        Assert.Equal(0, plusFiveDexterityVest.Quality);
+    }
+}
---
+++ csharp.xUnit/GildedRoseTests/GildedRose_TestBrie.cs
@@ -0,0 +1,59 @@
+﻿using Xunit;
+using System.Collections.Generic;
+using GildedRoseKata;
+using GildedRoseTests.ApprovalTests.AgedBrie;
+
+namespace GildedRoseTests;
+
+public class GildedRose_TestBrie
+{
+    [Fact]
+    public void AgedBrie_Increases_Quality_By_Day_By_One()
+    {
+        var brie = new Item { Name = "Aged Brie", SellIn = 2, Quality = 0 };
+        List<Item> items = [brie];
+
+        var gildedRose = new GildedRose(items);
+        gildedRose.UpdateQuality();
+        
+        Assert.Equal(1, brie.Quality);
+    }
+    
+    [Fact]
+    public void AgedBrie_Decreases_SellIn_By_Day_By_One()
+    {
+        var brie = new Item { Name = "Aged Brie", SellIn = 2, Quality = 0 };
+        List<Item> items = [brie];
+
+        var gildedRose = new GildedRose(items);
+        gildedRose.UpdateQuality();
+        
+        Assert.Equal(1, brie.SellIn);
+    }
+    
+    
+    [Fact]
+    public void AgedBrie_Increases_Quality_After_SellIn_Gets_ZeroOrNegative_By_Two()
+    {
+        var brie = new Item { Name = "Aged Brie", SellIn = 0, Quality = 0 };
+        List<Item> items = [brie];
+
+        var gildedRose = new GildedRose(items);
+        gildedRose.UpdateQuality();
+        
+        Assert.Equal(2, brie.Quality);
+    }
+    
+    [Fact]
+    public void AgedBrie_Quality_Doesnt_Increase_Beyond_50()
+    {
+        var brie = new Item { Name = "Aged Brie", SellIn = 0, Quality = 50 };
+        List<Item> items = [brie];
+
+        var gildedRose = new GildedRose(items);
+        gildedRose.UpdateQuality();
+        
+        Assert.Equal(50, brie.Quality);
+    }
+}
+// PlusFiveDexterityVest
