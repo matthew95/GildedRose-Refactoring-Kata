09c25a242c8e54e8a5d9d8182501f0bb4ea93aea
Guido Klijn <guidoklijn42@gmail.com>
2025-07-27 01:26:49 +0200

    move ItemNames out of the way so that i can continue refactoring

--- csharp.xUnit/GildedRose/GildedRose.cs
+++ csharp.xUnit/GildedRose/GildedRose.cs
@@ -10,16 +10,6 @@ public class GildedRose
     {
         this.Items = Items;
     }
-    private static class ItemNames
-    {
-        public static string AgedBrie => "Aged Brie";
-        public static string BackstagePasses => "Backstage passes to a TAFKAL80ETC concert";
-        public static string Conjured => "Conjured Mana Cake";
-        public static string ElixirOfTheMongoose => "Elixir of the Mongoose";
-        public static string PlusFiveDexterityVest => "+5 Dexterity Vest";
-        public static string Sulfuras => "Sulfuras, Hand of Ragnaros";
-        
-    }
 
     public void UpdateQuality()
     {
---
+++ csharp.xUnit/GildedRose/ItemNames.cs
@@ -0,0 +1,12 @@
+namespace GildedRoseKata;
+
+public static class ItemNames
+{
+    public static string AgedBrie => "Aged Brie";
+    public static string BackstagePasses => "Backstage passes to a TAFKAL80ETC concert";
+    public static string Conjured => "Conjured Mana Cake";
+    public static string ElixirOfTheMongoose => "Elixir of the Mongoose";
+    public static string PlusFiveDexterityVest => "+5 Dexterity Vest";
+    public static string Sulfuras => "Sulfuras, Hand of Ragnaros";
+    
+}
